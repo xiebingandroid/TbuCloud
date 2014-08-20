e80d105ac574a6273004f77ed9b6891638864e2b
xiebingandroid <mengchanghuan2009@163.com>
2014-08-20 15:53:01 +0800

    交叉推荐功能改进

--- Project/src/com/tallbigup/android/cloud/Top20RankInfoCallback.java
+++
@@ -1,8 +0,0 @@
-package com.tallbigup.android.cloud;
-
-import java.util.List;
-import java.util.Map;
-
-public interface Top20RankInfoCallback {
-	void result(final boolean success,final List<Map<String,String>> callback);
-}
