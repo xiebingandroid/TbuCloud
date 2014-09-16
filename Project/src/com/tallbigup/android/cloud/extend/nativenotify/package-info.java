f2254a021cab081a7fe71f299781121e9495d6e4
moloxiao <molo.xiao@gmail.com>
2014-09-16 10:07:37 +0800

    更新本地提醒注释信息

--- Project/src/com/tallbigup/android/cloud/extend/nativenotify/package-info.java
+++ Project/src/com/tallbigup/android/cloud/extend/nativenotify/package-info.java
@@ -7,8 +7,8 @@
  * AndroidManifest.xml的设置
  * 
  *  <receiver android:name="com.tallbigup.android.cloud.extend.nativenotify.NativeNotifyReceiver" >
-        <meta-data android:name="package_info" android:value="your game id here" />
-        	<meta-data android:name="launch_info" android:value="your appid here" />
+        <meta-data android:name="package_info" android:value="填写你的包名" />
+        	<meta-data android:name="launch_info" android:value="填写你的入口信息" />
         	<meta-data android:name="push_icon" android:resource="@drawable/push_logo" />
  *  </receiver>
  *
