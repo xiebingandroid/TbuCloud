2c254db4b56f2c0d208c6d5571792fcd5bd12510
Molo Xiao <molo.xiao@gmail.com>
2015-05-13 08:55:51 +0800

    格式备份

--- Project/src/com/tallbigup/android/cloud/TbuCloud.java
+++ Project/src/com/tallbigup/android/cloud/TbuCloud.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.ComponentName;
